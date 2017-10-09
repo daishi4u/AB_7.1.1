ea608cc555038c6b12638be8ae2d5ae04cd44f1d
Brett Wagner <daishi4u@yahoo.com>
2017-10-08 22:23:07 -0500

    change to intelliactive as default governor

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -405,6 +405,9 @@ extern struct cpufreq_governor cpufreq_gov_interactive;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_CAFACTIVE)
 extern struct cpufreq_governor cpufreq_gov_cafactive;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_cafactive)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTELLIACTIVE)
+extern struct cpufreq_governor cpufreq_gov_intelliactive;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_intelliactive)
 #endif
 
 
